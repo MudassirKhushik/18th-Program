ea5a6f0eb447d0c5b082b7d10b5a7bd234c77344
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-28 21:38:49 +0500

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,4 +1,5 @@
 //Arithmetic operations
+
 #include <iostream>
 
 using namespace std;
@@ -12,13 +13,6 @@ int main () {
 		cout << (5 + 6) * 2 << endl;
 		cout << 10.0 / 3.0 << endl;
 		cout << 15.0 + 2.55 << endl;
-		
-		int wnum = 15;
-		double dnum = 2.55;
-		
-		wnum ++;
-		
-		cout << wnum << endl ;
 		 
 	return 0;
 	
