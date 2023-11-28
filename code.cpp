a2cefacb7187a92a2592a01ba9cef0bacacd5416
Mudassir Khushik <150449494+MudassirKhushik@users.noreply.github.com>
2023-11-28 21:37:36 +0500

    Create code.cpp

---
+++ code.cpp
@@ -0,0 +1,25 @@
+//Arithmetic operations
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+		cout << 5 + 4  << endl;
+		cout << 5 - 3  << endl;
+		cout << 5 * 2  << endl;
+		cout << 5 + 6 * 2 << endl;
+		cout << (5 + 6) * 2 << endl;
+		cout << 10.0 / 3.0 << endl;
+		cout << 15.0 + 2.55 << endl;
+		
+		int wnum = 15;
+		double dnum = 2.55;
+		
+		wnum ++;
+		
+		cout << wnum << endl ;
+		 
+	return 0;
+	
+}
